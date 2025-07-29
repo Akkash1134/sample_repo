6d6881649c71f4e696f0781ea3f45d0258474bbb
akkash1134 <akkashak1418@gmail.com>
2025-07-29 07:35:13 +0530

    4th line added

--- create hello .java
+++ create hello .java
@@ -1,13 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-<<<<<<< HEAD
+
         System.out.println("this is my second line from eclipse");
-=======
-adding third line from eclipse 
+//adding third line from eclipse 
 System.out.println("3rd line");
 
-     //   im updating from git
->>>>>>> branch 'main' of https://github.com/Akkash1134/sample_repo.git
+//yes im removed unnecessary code in this file.
+System.out.println("last line im added from eclipse");
+
+
     }
 }
