95df9be33c6ba5a8129476061df5658158db90e8
akkash1134 <akkashak1418@gmail.com>
2025-07-29 07:32:21 +0530

    3rd line from eclipse

--- create hello .java
+++ create hello .java
@@ -4,6 +4,8 @@ public class HelloWorld {
 <<<<<<< HEAD
         System.out.println("this is my second line from eclipse");
 =======
+adding third line from eclipse 
+System.out.println("3rd line");
 
      //   im updating from git
 >>>>>>> branch 'main' of https://github.com/Akkash1134/sample_repo.git
